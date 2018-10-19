b0a08c575e8fd5553d9c0dad404a8d6f36ce07c3
ninedraft <clamores.pro@gmail.com>
2018-10-19 12:50:01 +0300

    add events pagination method #36

--- pkg/db/events.go
+++ pkg/db/events.go
@@ -45,6 +45,32 @@ func (mongo *MongoStorage) GetEventsInNamespacesList(resourcetype model.Resource
 	return result, nil
 }
 
+// Returns paginated event list (pageSize events per page).
+// If pageN < 0, then uses default value 0.
+// If pageSize < 1, then uses default value 32
+// Results are sorted from newest to oldest.
+func (mongo *MongoStorage) GetEventsInNamespacesListPage(resourcetype model.ResourceType, pageN, pageSize int, namespaces ...string) ([]model.Event, error) {
+	if pageN < 0 {
+		pageN = 0
+	}
+	if pageSize < 1 {
+		pageSize = 32
+	}
+	mongo.logger.WithField("collection", mongodb.EventsCollection).Debugf("getting events page %d(%d per page) in namespace", pageN, pageSize)
+	var collection = mongo.db.C(mongodb.EventsCollection)
+	result := make([]model.Event, 0)
+	if err := collection.Find(bson.M{
+		"resourcenamespace": bson.M{
+			"$in": namespaces,
+		},
+		"resourcetype": resourcetype,
+	}).Sort("-eventtime").Skip(pageN * pageSize).Limit(pageSize).All(&result); err != nil {
+		mongo.logger.WithError(err).Errorf("unable to get events page %d(%d per page) in namespace", pageN, pageSize)
+		return nil, PipErr{error: err}.ToMongerr().NotFoundToNil().Extract()
+	}
+	return result, nil
+}
+
 func (mongo *MongoStorage) GetAllEventsList(resourcetype model.ResourceType, limit int, startTime time.Time) ([]model.Event, error) {
 	mongo.logger.WithField("collection", mongodb.EventsCollection).Debugf("getting events in all namespaces")
 	var collection = mongo.db.C(mongodb.EventsCollection)
