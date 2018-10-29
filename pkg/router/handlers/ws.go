e0d9f38c94d9464331bb4f0cf88c428761511656
Margarita <margo.tuleninova@gmail.com>
2018-10-29 13:15:15 +0300

    fix websockets

--- pkg/router/handlers/ws.go
+++ pkg/router/handlers/ws.go
@@ -3,20 +3,22 @@ package handlers
 import (
 	"context"
 	"net/http"
-	"runtime"
 	"sort"
 	"sync"
 	"time"
 
-	"github.com/containerum/events-api/pkg/eventfilter"
-	"github.com/containerum/events-api/pkg/model"
 	"github.com/containerum/events-api/pkg/util/atomicbool"
+
+	"github.com/containerum/events-api/pkg/model"
+
+	"github.com/ninedraft/gocontrol"
+
 	"github.com/containerum/events-api/pkg/util/ticker"
-	kubemodel "github.com/containerum/kube-client/pkg/model"
+	kubeModel "github.com/containerum/kube-client/pkg/model"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
-	"github.com/ninedraft/gocontrol"
-	"github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 var upgrader = websocket.Upgrader{
@@ -28,89 +30,80 @@ var upgrader = websocket.Upgrader{
 func withWS(ginCtx *gin.Context, params model.FuncParams, dbPeriod time.Duration, getfuncs ...model.EventsFunc) {
 	var control = &gocontrol.Guard{}
 	defer control.Wait()
-
-	//Divide limit to all functions
-
-	var filter eventfilter.Predicate = eventfilter.True
-	var userDefinedLevelWhiteList = ginCtx.QueryArray("levels")
-	var userDefinedSourceWhiteList = ginCtx.QueryArray("resources")
-
-	if len(userDefinedLevelWhiteList) > 0 || len(userDefinedSourceWhiteList) > 0 {
-		var levelWhiteList = func() []kubemodel.EventKind {
-			var kinds = make([]kubemodel.EventKind, 0, len(userDefinedLevelWhiteList))
-			for _, level := range userDefinedLevelWhiteList {
-				kinds = append(kinds, kubemodel.EventKind(level))
-			}
-			return kinds
-		}()
-
-		var resourceWhiteList = func() []kubemodel.ResourceType {
-			var resources = make([]kubemodel.ResourceType, 0, len(userDefinedSourceWhiteList))
-			for _, resource := range userDefinedSourceWhiteList {
-				resources = append(resources, kubemodel.ResourceType(resource))
-			}
-			return resources
-		}()
-
-		if len(userDefinedLevelWhiteList) > 0 {
-			filter = eventfilter.MatchAnyKind(levelWhiteList...)
-		}
-		if len(userDefinedSourceWhiteList) > 0 {
-			filter = eventfilter.And(filter, eventfilter.MatchAnyType(resourceWhiteList...))
-		}
-	}
-
-	conn, err := upgrader.Upgrade(ginCtx.Writer, ginCtx.Request, nil)
+	c, err := upgrader.Upgrade(ginCtx.Writer, ginCtx.Request, nil)
 	if err != nil {
-		logrus.Debug(err)
+		log.Debug(err)
 		return
 	}
-	defer conn.Close()
-	var errChan = make(chan error)
-	var unfilteredEvents = make(chan kubemodel.Event)
-	defer close(unfilteredEvents)
-	defer close(errChan)
+	defer c.Close()
 
-	conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-	conn.SetPongHandler(func(string) error {
-		conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	ctx, cancelCTX := context.WithCancel(ginCtx.Request.Context())
+	defer cancelCTX()
+
+	var errChan = make(chan error)
+	var resultChan = make(chan kubeModel.Event)
+	var finalChan = make(chan []kubeModel.Event)
+	defer close(resultChan)
+	defer close(errChan)
+	defer close(finalChan)
+
+	var firstTimeWG = &sync.WaitGroup{}
+
+	c.SetReadDeadline(time.Now().Add(10 * time.Second))
+	c.SetPongHandler(func(string) error {
+		c.SetReadDeadline(time.Now().Add(10 * time.Second))
 		return nil
 	})
 
 	//Checking for closed connection
-	go CheckConnection(control, conn, errChan)
+	go func() {
+		defer control.Go()()
+		for {
+			_, _, err := c.ReadMessage()
+			if err != nil {
+				errChan <- err
+				return
+			}
+		}
+	}()
 
 	//Limiter. Waiting for all DB request to finish on first run.
 	for _, eventSource := range getfuncs {
+		firstTimeWG.Add(1)
 		go EventAggregator{
-			CTX:         ginCtx,
-			EventSource: eventSource,
-			EventDrain:  unfilteredEvents,
-			ErrChan:     errChan,
-			Control:     control,
+			CTX:                ctx,
+			Params:             params,
+			EventSource:        eventSource,
+			EventDrain:         resultChan,
+			ErrChan:            errChan,
+			Control:            control,
+			DBPeriod:           dbPeriod,
+			FirstTimeWaitGroup: firstTimeWG,
 		}.Run()
 	}
 
-	var filteredEvents = filter.Pipe(-1, unfilteredEvents)
-	var eventBatches = make(chan []kubemodel.Event)
 	go EventBatcher{
-		Ctx:               ginCtx,
-		ErrChan:           errChan,
-		Quant:             1 * time.Second,
-		BatchDrain:        eventBatches,
-		PreallocBatchSize: 16,
-		EventSource:       filteredEvents,
+		Ctx:                ctx,
+		ErrChan:            errChan,
+		Quant:              1 * time.Second,
+		BatchDrain:         finalChan,
+		PreallocBatchSize:  16,
+		EventSource:        resultChan,
+		Control:            control,
+		FirstTimeWaitGroup: firstTimeWG,
 	}.Run()
 
 	go func() {
 		defer control.Go()()
+
+		var limitOnce = sync.Once{}
 
 		pingTicker := ticker.NewTicker(1 * time.Second)
 		pingTicker.Start()
 		defer pingTicker.Stop()
 		for {
 			select {
-			case result, ok := <-eventBatches:
+			case result, ok := <-finalChan:
 				if !ok {
 					return
 				}
@@ -120,13 +113,19 @@ func withWS(ginCtx *gin.Context, params model.FuncParams, dbPeriod time.Duration
 						timej, _ := time.Parse(time.RFC3339, result[j].Time)
 						return timei.After(timej)
 					})
-					logrus.Infof("Writing %v events", len(result))
-					if err := conn.WriteJSON(result); err != nil {
+					limitOnce.Do(func() {
+						if params.Limit < len(result) && params.Limit > 0 {
+							result = result[:params.Limit]
+						}
+					})
+
+					log.Infof("Writing %v events", len(result))
+					if err := c.WriteJSON(result); err != nil {
 						errChan <- err
 					}
 				}
 			case <-pingTicker.Ticks():
-				if err := conn.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				if err := c.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
 					return
 				}
 			}
@@ -135,7 +134,7 @@ func withWS(ginCtx *gin.Context, params model.FuncParams, dbPeriod time.Duration
 
 	for selecerr := range errChan {
 		if selecerr != nil {
-			logrus.Debug(selecerr)
+			log.Debug(selecerr)
 			return
 		}
 	}
@@ -145,8 +144,8 @@ type EventBatcher struct {
 	Ctx                context.Context
 	ErrChan            chan error
 	Quant              time.Duration
-	EventSource        <-chan kubemodel.Event
-	BatchDrain         chan<- []kubemodel.Event
+	EventSource        <-chan kubeModel.Event
+	BatchDrain         chan<- []kubeModel.Event
 	Control            *gocontrol.Guard
 	PreallocBatchSize  int
 	FirstTimeWaitGroup *sync.WaitGroup
@@ -165,7 +164,7 @@ func (batcher EventBatcher) Run() {
 		ready()
 	}()
 
-	results := make([]kubemodel.Event, 0)
+	results := make([]kubeModel.Event, 0)
 	var timer = ticker.NewTicker(batcher.Quant)
 	timer.Start()
 	defer timer.Stop()
@@ -179,7 +178,7 @@ func (batcher EventBatcher) Run() {
 				continue
 			}
 			finalChan <- results
-			results = make([]kubemodel.Event, 0)
+			results = make([]kubeModel.Event, 0)
 		case event, ok := <-resultChan:
 			if !ok {
 				return
@@ -193,7 +192,7 @@ type EventAggregator struct {
 	CTX                context.Context
 	Params             model.FuncParams
 	EventSource        model.EventsFunc
-	EventDrain         chan<- kubemodel.Event
+	EventDrain         chan<- kubeModel.Event
 	ErrChan            chan<- error
 	Control            *gocontrol.Guard
 	DBPeriod           time.Duration
@@ -237,32 +236,6 @@ func (aggregate EventAggregator) Run() {
 			//Get only new events
 			params.StartTime = time.Now()
 			time.Sleep(dbPeriod)
-		}
-	}
-}
-
-func AbortWaiter(aborted func() bool) <-chan struct{} {
-	var wait = make(chan struct{})
-	go func() {
-		defer close(wait)
-		for {
-			if aborted() {
-				return
-			}
-			runtime.Gosched()
-			time.Sleep(100 * time.Millisecond)
-		}
-	}()
-	return wait
-}
-
-func CheckConnection(control *gocontrol.Guard, conn *websocket.Conn, errChan chan<- error) {
-	defer control.Go()()
-	for {
-		_, _, err := conn.ReadMessage()
-		if err != nil {
-			errChan <- err
-			return
 		}
 	}
 }
